402e320302537f33fd789a863818c25131d4a10a
Jaya Vishwakarma <jayavish423@gmail.com>
2023-04-03 14:03:24 +0530

    first commit

---
+++ MyApp/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+int n = 6;
+for (int i = 3; i <= n; i++)
+{
+    for (int j = 1; j <= i; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
+
+
+for (int i = n; i >= 3; i--)
+{
+    for (int j = 1; j <= i; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
